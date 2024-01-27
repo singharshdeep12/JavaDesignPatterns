ebbc77b4c8f12d9f86e355bc7d22212568dd58ab
Arshdeep Singh <2016csb1039@iitrpr.ac.in>
2024-01-27 23:23:40 +0530

    builder2 added

---
+++ src/builder/Builder2.java
@@ -0,0 +1,81 @@
+package builder;
+
+public class Builder2 {
+}
+
+
+class UserEntity2{
+
+    private String name;
+    private int age;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    private void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    private void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+
+    public static UserBuilder1 getBuilder(){
+        return new UserBuilder1();
+    }
+
+    static class UserBuilder1{
+        private UserEntity user;
+
+        UserBuilder1(){
+            user = new UserEntity();
+        }
+
+        public UserBuilder1 withName(String name){
+            user.setName(name);
+            return this;
+        }
+
+        public UserBuilder1 withAge(int age){
+            user.setAge(age);
+            return this;
+        }
+
+        public UserBuilder1 withAddress(String address){
+            user.setAddress(address);
+            return this;
+        }
+
+        public UserEntity buildUser(){
+            return user;
+        }
+
+    }
+
+
+}
+
